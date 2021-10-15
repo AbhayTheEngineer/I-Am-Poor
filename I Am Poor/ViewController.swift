dd26a2974572784fcfa96b4cb0dae16ef9851418
Abhay Agarwal <abhay.agarwal97@hotmail.com>
2021-10-15 16:18:16 +0400

    Committing files.

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Abhay Agarwal on 28/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
